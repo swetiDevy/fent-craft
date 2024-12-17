21b17aaa092523ac2ea634fc94f376204373bd10
sweti <blazej.odzimek@gmail.com>
2024-12-17 21:26:41 +0100

    emmmmm  added nothing basicly bc i cant code and idk how to get loot table from minercaft

---
+++ src/main/java/swetidev/fentcraft/util/ModLootTableModifiers.java
@@ -0,0 +1,10 @@
+package swetidev.fentcraft.util;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
+
+public class ModLootTableModifiers {
+    private static final Identifier POPPY_ID = Blocks.POPPY.getLootTableId();
+
+
+}
